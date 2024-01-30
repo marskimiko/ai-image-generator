182aad2c1fea2846b0dddde0814a1feaf464e7fa
Marisa Saunders <marisaksaunders@gmail.com>
2024-01-30 13:23:28 -0500

    add surprise options array

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
 const App = () => {
+  const surpriseOptions = [
+    'A blue ostrich eating melon',
+    'A matisse style shark on the telephone',
+    'A pineapple sunbathing on an island'
+  ]
+
   return (
     <div className="app">
-
+      <section className="search-section">
+        <p>Please enter a detailed description 
+          <span className="surprise">Surprise me</span>
+        </p>
+      </section>
+      <section className="image-section"></section>
     </div>
   );
 }
