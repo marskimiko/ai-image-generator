d6428fd953e75c717229f6946a8d8efb8ec83264
Marisa Saunders <marisaksaunders@gmail.com>
2024-01-31 12:54:16 -0500

    update getImages

--- src/App.js
+++ src/App.js
@@ -1,4 +1,8 @@
+import { useState } from 'react';
+
 const App = () => {
+  const [images, setImages] = useState(null);
+  const [value, setValue] = useState("");
   const surpriseOptions = [
     'A blue ostrich eating melon',
     'A matisse style shark on the telephone',
@@ -18,11 +22,16 @@ const App = () => {
       }
     const response = await fetch('http://localhost:8000/images', options)
     const data = await response.json();
+    debugger
     console.log('data:',data)
+    setImages(data)
+    console.log('images',images)
     } catch (error) {
       console.error(error);
     }
   }
+
+  console.log('value',value)
 
   return (
     <div className="app">
@@ -31,13 +40,19 @@ const App = () => {
           <span className="surprise">Surprise me</span>
         </p>
         <div className="input-container">
-          <input 
+          <input
+            value={value} 
             placeholder="A cat dancing in the rain under the milky way..."
+            onChange={e => setValue(e.target.value)}
           />
           <button onClick={getImages}>Generate</button>
         </div>
       </section>
-      <section className="image-section"></section>
+      <section className="image-section">
+        {images?.map((image, _index) => (
+          <img key={_index} src={image.url} alt={`Generated pic of ${value}`}/>
+        ))}
+      </section>
     </div>
   );
 }
